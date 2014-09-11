5a055a7572561dd1855336a99e41ec5626a4a12a
Erik Guffrey <erik.guffrey@gmail.com>
2014-09-11 18:26:32 -0400

    added in shelljs and fs so that we can create new projects from the github master repository

--- yote-cli.js
+++ yote-cli.js
@@ -1,14 +1,26 @@
 #!/usr/bin/env node
 
-var program = require('commander');
+var program = require('commander')
+    , fs    = require('fs')
+    , chalk = require('chalk')
+    , shell = require('shelljs')
+  ;
 
 program
     .version('0.0.1')
     .usage('<keywords>')
+    .option('-c, --create [appName]', 'Create a new Yote applicaion directory called [appName]')
     .parse(process.argv);
 
-if(!program.args.length) {
-    program.help();
-} else {
-    console.log('Keywords: ' + program.args);   
+if(program.create) {
+  shell.exec("git clone https://github.com/fugitivelabsgit/yote.git " + program.create);
+  console.log(chalk.bgCyan("Success!"));
+  console.log(chalk.green("Now cd into " + program.create + " and run ") + chalk.bgGreen("[sudo] npm install") + chalk.green(" to get started"));
+  process.exit(0);
 }
+
+if(!program.args.length) {
+  program.help();
+}
+
+
