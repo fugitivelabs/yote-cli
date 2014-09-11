5701e15b9b3a1af61d7e2e3c3ccf2374bf1f535a
Erik Guffrey <erik.guffrey@gmail.com>
2014-09-11 18:31:11 -0400

    howl

--- yote-cli.js
+++ yote-cli.js
@@ -10,12 +10,18 @@ program
     .version('0.0.1')
     .usage('<keywords>')
     .option('-c, --create [appName]', 'Create a new Yote applicaion directory called [appName]')
+    .option('-H --howl', '')
     .parse(process.argv);
 
 if(program.create) {
   shell.exec("git clone https://github.com/fugitivelabsgit/yote.git " + program.create);
   console.log(chalk.bgCyan("Success!"));
   console.log(chalk.green("Now cd into " + program.create + " and run ") + chalk.bgGreen("[sudo] npm install") + chalk.green(" to get started"));
+  process.exit(0);
+}
+
+if(program.howl) {
+  shell.exec("say 'owooooooooooooooo'");
   process.exit(0);
 }
 
